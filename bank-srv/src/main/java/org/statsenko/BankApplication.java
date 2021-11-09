d123fa19ea50631dd768d0974c97c3fa19ed9be8
vladstatsenko <rfhfcbrbQ1>
2021-11-09 13:29:52 +0300

    added eureka

--- bank-srv/src/main/java/org/statsenko/BankApplication.java
+++ bank-srv/src/main/java/org/statsenko/BankApplication.java
@@ -2,7 +2,9 @@ package org.statsenko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BankApplication {
     public static void main(String[] args) {
