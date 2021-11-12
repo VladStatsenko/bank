d208419de2a6c0ac189b1ff9b6cd29b9d2a3786c
vladstatsenko <rfhfcbrbQ1>
2021-11-12 15:54:38 +0300

    added liquebase

--- bank-api/src/main/java/dto/request/ClientDto.java
+++ bank-api/src/main/java/dto/request/ClientDto.java
@@ -1,9 +1,11 @@
 package dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -19,6 +21,8 @@ public class ClientDto implements Serializable {
     @JsonProperty("mid_name")
     private String midName;
     @JsonProperty("birth_date")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
     private Date birthDate;
     private String tin;
     private String login;
--- bank-db/src/main/java/org/statsenko/entity/Profile.java
+++ bank-db/src/main/java/org/statsenko/entity/Profile.java
@@ -26,6 +26,7 @@ public class Profile extends AbstractEntity {
 
     private String login;
     private String password;
+    @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
